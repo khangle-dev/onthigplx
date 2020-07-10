aab024216f81bbc533089af97bc846505c96b480
khangle-dev <66984755+khangle-dev@users.noreply.github.com>
2020-07-10 14:32:54 +0700

    1.8

--- controllers/violation.js
+++ controllers/violation.js
@@ -1,28 +1,28 @@
-app.controller("violationCtrl", function ($scope) {
+app.controller("violationCtrl", function($scope) {
     $scope.violationNo = getParaCurr("index")
     $scope.violationGroup = getParaCurr("violationGroup")
-    
-    $scope.topic = fullLawTopics.filter(function(law){
+
+    $scope.topic = fullLawTopics.filter(function(law) {
         return law.code == $scope.violationGroup
     })[0]
 
-    $scope.violation = fullViolations.filter(function(violation){
+    $scope.violation = fullViolations.filter(function(violation) {
         return violation.no == $scope.violationNo
     })[0]
 
-    $scope.list = fullViolations.filter(function(violation){
-        return $scope.violation.relations.includes(violation.no) 
+    $scope.list = fullViolations.filter(function(violation) {
+        return $scope.violation.relations.includes(violation.no)
     })
 
-    $scope.bookmarks1 = $scope.violation.bookmarks.filter (function(book){
+    $scope.bookmarks1 = $scope.violation.bookmarks.filter(function(book) {
         return book.bookmarkType == 1
     })
 
-    $scope.bookmarks2 = $scope.violation.bookmarks.filter (function(book){
+    $scope.bookmarks2 = $scope.violation.bookmarks.filter(function(book) {
         return book.bookmarkType == 2
     })
 
-    $scope.bookmarks3 = $scope.violation.bookmarks.filter (function(book){
+    $scope.bookmarks3 = $scope.violation.bookmarks.filter(function(book) {
         return book.bookmarkType == 3
     })
 });
